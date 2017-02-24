a647ba2a2222a0bc2d70bd57f839e2384778bbb1
calae91 <romain.quijal@hotmail.fr>
2017-02-24 15:30:52 +0100

    Update principal.java

--- l2/Maison/principal.java
+++ l2/Maison/principal.java
@@ -2,8 +2,8 @@
 /**
  * Décrivez votre classe principal ici.
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author (calae91)
+ * @version (1.0)
  */
 public class principal
 {
